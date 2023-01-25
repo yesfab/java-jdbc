83268188cb15a57a7811d90330ddc24962485307
Bibou <jordan.bouville@edu.esiee-it.com>
2023-01-25 12:34:58 +0100

    [UPDATE]

--- src/main/java/com/coding/modulo/Creer.java
+++ src/main/java/com/coding/modulo/Creer.java
@@ -1,4 +1,21 @@
 package com.coding.modulo;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Creer {
+
+    public static void create(Statement stmt) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the name of the table: ");
+        String table = sc.nextLine();
+        System.out.print("Enter the columns you want to insert data into: ");
+        String columns = sc.nextLine();
+        System.out.print("Enter the values you want to insert: ");
+        String values = sc.nextLine();
+        String sql = "INSERT INTO " + table + " (" + columns + ") VALUES (" + values + ")";
+        stmt.executeUpdate(sql);
+        System.out.println("Record created successfully.");
+    }
 }
